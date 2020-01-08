08e726300f6395f753872bcbe801b321666ae8dd
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-08 17:40:46 +0100

    eccolo

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 // lista cognomi
 var cognomi = ['Rand', 'Stark', 'Banner', 'Murdock', 'Roger'];
 console.log(cognomi);
+
+// Inserimento cognome Utente
+var cognomeUtente = prompt('Inserisci il tuo cognome');
+cognomeMaiuscolo = cognomeUtente.toUpperCase(0);
+cognomi.push(cognomeMaiuscolo);
+console.log(cognomi);
+
+// Ordinare alfabeticamente
+cognomi.sort();
+console.log(cognomi);
