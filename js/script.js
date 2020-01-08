0e166157d6ce32bfc28a0dac1f84f853d7d8b8ae
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-08 18:59:16 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -6,10 +6,18 @@ console.log(cognomi);
 
 // Inserimento cognome Utente
 var cognomeUtente = prompt('Inserisci il tuo cognome');
-cognomeMaiuscolo = cognomeUtente.toUpperCase(0);
+cognomeMaiuscolo = cognomeUtente.charAt().toUpperCase() + cognomeUtente.substring(1);
 cognomi.push(cognomeMaiuscolo);
 console.log(cognomi);
 
 // Ordinare alfabeticamente
 cognomi.sort();
 console.log(cognomi);
+ for (var i = 0; i < cognomi.length; i++) {
+   document.getElementById('lista') .innerHTML  += '<li>' + cognomi[i] + '</li>';
+ }
+
+var posizione = cognomi.indexOf(cognomeUtente) + 1;
+ document.getElementById('posizione') .innerHTML  = "Il cognome da te inserito nella " + posizione + " dell'elenco.";
+
+document.getElementById('lista').classList.add('list_style');
